1bc827254556fce9956b999271e91c5a501b0984
mwl4 <mwl450@gmail.com>
2018-02-28 23:27:47 +0100

    Fixed copy_bin_files script.

--- src/gcc_rtti/copy_bin_files.bat
+++ src/gcc_rtti/copy_bin_files.bat
@@ -1,2 +1,2 @@
 rem Use this script to copy binary files to /plugins directory in IDA
-copy "..\..\bin\win32\gcc_rtti.plw" "..\..\"
+copy "..\..\bin\win32\gcc_rtti.dll" "..\..\"
--- src/gcc_rtti/copy_bin_files_64.bat
+++ src/gcc_rtti/copy_bin_files_64.bat
@@ -1,2 +1,2 @@
 rem Use this script to copy binary files to /plugins directory in IDA
-copy "..\..\bin\win64\gcc_rtti.p64" "..\..\"
+copy "..\..\bin\win64\gcc_rtti64.dll" "..\..\"
